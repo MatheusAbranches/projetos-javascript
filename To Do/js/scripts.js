d2f128c956b55ab7222aaad489107dc36b7b3acf
Matheus Abranches <matheus-abranches@hotmail.com>
2022-08-18 10:18:28 -0300

    To Do

---
+++ To Do/js/scripts.js
@@ -0,0 +1,72 @@
+// função que add tarefa
+function addTask() {
+  // titulo da tarefa
+  const taskTitle = document.querySelector("#task-title").value;
+
+  if (taskTitle) {
+    //   clona template
+    const template = document.querySelector(".template");
+
+    const newTask = template.cloneNode(true);
+
+    // add titulo
+
+    newTask.querySelector(".task-title").textContent = taskTitle;
+
+    //  remover as classes desnecessárias
+
+    newTask.classList.remove("template");
+    newTask.classList.remove("hide");
+
+    // adiciona tarefa na lista
+
+    const list = document.querySelector("#task-list");
+
+    list.appendChild(newTask);
+
+    // adicionar evento de remover
+
+    const removebtn = newTask
+      .querySelector(".remove-btn")
+      .addEventListener("click", function () {
+        removeTask(this);
+      });
+
+    // Adicionar evento de completar tarefa
+
+    const doneBtn = newTask
+      .querySelector(".done-btn")
+      .addEventListener("click", function () {
+        completeTask(this);
+      });
+
+    // limpar texto
+
+    document.querySelector("#task-title").value = "";
+  }
+}
+
+// função de completar tarefa
+
+function completeTask(task) {
+  
+  const taskTocomplete = task.parentNode;
+
+  taskTocomplete.classList.toggle("done")
+}
+
+// função de remover tarefa
+
+function removeTask(task) {
+  task.parentNode.remove(true);
+}
+
+// evento de add tarefa
+const addbtn = document.querySelector("#add-btn");
+
+addbtn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  addTask();
+});
+
